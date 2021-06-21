45d8cd33e0867a4a063fdd666fa21be68064506e
erobbi <elirobbins0@gmail.com>
2021-06-20 22:53:56 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats[cats.length] = "Ralph"; 
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats[cats.slice(2,2)]
+}
+
+    
